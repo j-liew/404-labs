f7948be7afe35a0dc78c4f60257a8be868697a5a
Justin Liew <justinwcliew@gmail.com>
2020-01-07 10:43:40 -0500

    print self

--- lab1/print-req-ver.py
+++ lab1/print-req-ver.py
@@ -1,4 +1,8 @@
 import requests
 print (requests.__version__)
-google=requests.get("https://google.com/teapot")
-print(google)
+
+#google=requests.get("https://google.com/teapot")
+#print(google)
+
+res = requests.get ("https://raw.githubusercontent.com/j-liew/404-labs/master/lab1/print-req-ver.py")
+print(res.content)
